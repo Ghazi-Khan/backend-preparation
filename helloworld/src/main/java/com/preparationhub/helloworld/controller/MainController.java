a3c80e2d199a2bc973e90ada2cd3538092661d1b
Ghazi Khan <mailme.gkhan@gmail.com>
2024-05-08 13:53:48 +0530

    added path-variable examples

--- helloworld/src/main/java/com/preparationhub/helloworld/controller/MainController.java
+++ helloworld/src/main/java/com/preparationhub/helloworld/controller/MainController.java
@@ -3,6 +3,7 @@ package com.preparationhub.helloworld.controller;
 import com.preparationhub.helloworld.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -20,5 +21,19 @@ public class MainController {
     @GetMapping("/user")
     public User user() {
         return new User(101, "Ghazi khan", "ghazi@gmail.com");
+    }
+
+
+    @GetMapping("/{id}")
+    public String pathVariableExample1(@PathVariable String id) {
+        return "The path variable is => " + id;
+    }
+
+//    if we want to use different name for variable then the
+//    path-variable name then we need to pass the path-variable
+//    name inside @PathVariable annotation
+    @GetMapping("/user/{myId}")
+    String pathVariableExample2(@PathVariable("myId") String id) {
+        return "The path variable is => " + id;
     }
 }

11aae3e2f8abbcfa4ddd4437a3817a45679b2985
Ghazi Khan <mailme.gkhan@gmail.com>
2024-05-08 19:01:59 +0530

    ommiting email from response body by using JsonIgnore annotation

--- employee-app/src/main/java/com/learnhub/employeeapp/model/Employee.java
+++ employee-app/src/main/java/com/learnhub/employeeapp/model/Employee.java
@@ -1,11 +1,13 @@
 package com.learnhub.employeeapp.model;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@JsonIgnoreProperties({"email"})
 public class Employee {
     private String employeeId;
     private String firstName;

9c6a6c7c6da0b115a6f5bf2c10414393e9397608
Ghazi Khan <mailme.gkhan@gmail.com>
2024-05-08 13:36:01 +0530

    added method to return user object

--- helloworld/src/main/java/com/preparationhub/helloworld/controller/MainController.java
+++ helloworld/src/main/java/com/preparationhub/helloworld/controller/MainController.java
@@ -1,6 +1,8 @@
 package com.preparationhub.helloworld.controller;
 
+import com.preparationhub.helloworld.model.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -13,5 +15,10 @@ public class MainController {
     public String helloWorld() {
         visitors++;
         return "Hello world - you are the " + visitors + " visitor.";
+    }
+
+    @GetMapping("/user")
+    public User user() {
+        return new User(101, "Ghazi khan", "ghazi@gmail.com");
     }
 }

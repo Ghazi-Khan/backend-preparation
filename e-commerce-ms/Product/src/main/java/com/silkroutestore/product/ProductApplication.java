554591492ea88d65acede9ca003987afb1d3c881
Ghazi Khan <mailme.gkhan@gmail.com>
2024-05-09 14:30:23 +0530

    code refactoring

--- e-commerce-ms/Product/src/main/java/com/silkroutestore/product/ProductApplication.java
+++ e-commerce-ms/Product/src/main/java/com/silkroutestore/product/ProductApplication.java
@@ -1,4 +1,4 @@
-package com.silkroutestore.Product;
+package com.silkroutestore.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- e-commerce-ms/Product/src/test/java/com/silkroutestore/product/ProductApplicationTests.java
+++ e-commerce-ms/Product/src/test/java/com/silkroutestore/product/ProductApplicationTests.java
@@ -1,4 +1,4 @@
-package com.silkroutestore.Product;
+package com.silkroutestore.product;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

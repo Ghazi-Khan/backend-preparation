984adfe13691ddf01ae6a4564dad55f7898820de
Ghazi Khan <mailme.gkhan@gmail.com>
2024-05-12 23:08:32 +0530

    code refactoring - product-service

--- e-commerce-ms/product/src/main/java/com/silkroutestore/product/service/ProductService.java
+++ e-commerce-ms/product/src/main/java/com/silkroutestore/product/service/ProductService.java
@@ -2,7 +2,6 @@ package com.silkroutestore.product.service;
 
 import com.silkroutestore.product.model.ProductRequest;
 import com.silkroutestore.product.model.ProductResponse;
-import org.springframework.http.ResponseEntity;
 
 public interface ProductService {
     Long addProduct(ProductRequest productRequest);

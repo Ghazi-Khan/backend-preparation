9c2ff50f7f92e227af38d0c58e31acb203c43e80
Ghazi Khan <mailme.gkhan@gmail.com>
2024-05-14 13:58:58 +0530

    implemented circuit-breaker pattern using resilience4J for order-server

--- e-commerce-ms/order/src/main/java/com/silkroutestore/order/external/client/ProductService.java
+++ e-commerce-ms/order/src/main/java/com/silkroutestore/order/external/client/ProductService.java
@@ -1,11 +1,14 @@
 package com.silkroutestore.order.external.client;
 
+import com.silkroutestore.order.exception.CustomException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@CircuitBreaker(name = "external", fallbackMethod = "fallback")
 @FeignClient(name = "PRODUCT-SERVICE/product")
 public interface ProductService {
     @PutMapping("/reduceQuantity/{id}")
@@ -13,4 +16,12 @@ public interface ProductService {
             @PathVariable(name = "id") long productId,
             @RequestParam long quantity
     );
+
+    default ResponseEntity<Void> fallback(
+        @PathVariable(name = "id") long productId,
+        @RequestParam long quantity,
+        Exception e
+    ) {
+        throw new CustomException("Product service is not available", "UNAVAILABLE", 500);
+    }
 }
